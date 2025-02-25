e57940166ca0351fa1ac6f784f6fab6b2e997fd6
Braulio Rivas <brauliorivas2004@gmail.com>
2025-02-24 21:35:49 -0500

    jedi instead

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,5 +1,10 @@
 vim.lsp.start({
-	name = "python-lsp-server",
-	filetypes = { "py" },
-	cmd = { "pylsp" },
+	name = "jedi-language-server",
+	cmd = { "jedi-language-server" },
+	root_dir = vim.fs.dirname(
+		vim.fs.find(
+			{ "requirements.txt", "setup.py", "tox.ini", "requirements.txt", "Pipfile", "pyproject.toml" },
+			{ upward = true }
+		)[1]
+	),
 })
