12aaeee660bc7ba984c57e58f723ef8af5397a09
Braulio Rivas <brauliorivas2004@gmail.com>
2025-02-25 09:32:39 -0500

    pylsp with pipx

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,6 +1,6 @@
 vim.lsp.start({
 	name = "jedi-language-server",
-	cmd = { "jedi-language-server" },
+	cmd = { "pylsp" },
 	root_dir = vim.fs.dirname(
 		vim.fs.find(
 			{ "requirements.txt", "setup.py", "tox.ini", "requirements.txt", "Pipfile", "pyproject.toml" },
