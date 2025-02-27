fe6e1f4e1abc76ab3c3fb9355da946d3138323c3
Braulio Rivas <brauliorivas2004@gmail.com>
2025-02-27 17:30:44 -0500

    git status

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fg", builtin.git_status, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 			require("telescope").setup({
