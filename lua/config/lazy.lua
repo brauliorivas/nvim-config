bbd625033fca3bf64051e5a3c70e2c3d5cc79886
Braulio Rivas <brauliorivas2004@gmail.com>
2025-03-20 00:29:47 -0500

    dap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -107,5 +107,88 @@ require("actions-preview").setup({
 	),
 })
 
+local dap = require("dap")
 
-local none_ls = require("none-ls")
+dap.adapters.gdb = {
+	type = "executable",
+	command = "gdb",
+	args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+
+dap.configurations.c = {
+	{
+		name = "Launch",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtBeginningOfMainSubprogram = false,
+	},
+	{
+		name = "Select and attach to process",
+		type = "gdb",
+		request = "attach",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		pid = function()
+			local name = vim.fn.input("Executable name (filter): ")
+			return require("dap.utils").pick_process({ filter = name })
+		end,
+		cwd = "${workspaceFolder}",
+	},
+	{
+		name = "Attach to gdbserver :1234",
+		type = "gdb",
+		request = "attach",
+		target = "localhost:1234",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+	},
+}
+
+vim.keymap.set("n", "<F5>", function()
+	dap.continue()
+end)
+vim.keymap.set("n", "<F10>", function()
+	dap.step_over()
+end)
+vim.keymap.set("n", "<F11>", function()
+	dap.step_into()
+end)
+vim.keymap.set("n", "<F12>", function()
+	dap.step_out()
+end)
+vim.keymap.set("n", "<Leader>b", function()
+	dap.toggle_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>B", function()
+	dap.set_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>lp", function()
+	dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end)
+vim.keymap.set("n", "<Leader>dr", function()
+	dap.repl.open()
+end)
+vim.keymap.set("n", "<Leader>dl", function()
+	dap.run_last()
+end)
+vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+	require("dap.ui.widgets").hover()
+end)
+vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+	require("dap.ui.widgets").preview()
+end)
+vim.keymap.set("n", "<Leader>df", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set("n", "<Leader>ds", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end)
