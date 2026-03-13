a8a3b998f330041461754299fe51700513d775fc
Braulio Rivas <brauliorivas2004@gmail.com>
2026-03-13 18:32:36 -0500

    change to autopep8

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,7 +20,7 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'black' },
+      python = { 'autopep8' },
       javascript = { 'prettier', stop_after_first = true },
       c = { 'clang-format' },
       cpp = { 'clang-format' },
