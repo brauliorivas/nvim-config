47910f0743132937076564b90875907c6940048c
Braulio Rivas <brauliorivas2004@gmail.com>
2026-04-17 00:51:18 -0500

    single

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', branch = 'master' },
+  },
+  build = 'make tiktoken',
+  opts = {
+    temperature = 0.1, -- Lower = focused, higher = creative
+    trusted_tools = nil, -- Require approval for all tool calls
+    window = {
+      layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+      width = 0.3,
     },
-    build = 'make tiktoken',
-    opts = {
-      temperature = 0.1, -- Lower = focused, higher = creative
-      trusted_tools = nil, -- Require approval for all tool calls
-      window = {
-        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-        width = 0.3,
-      },
-      auto_insert_mode = true, -- Enter insert mode when opening
-    },
+    auto_insert_mode = true, -- Enter insert mode when opening
   },
 }
