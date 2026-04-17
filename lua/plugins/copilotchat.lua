6d59700cc6a8196ab785130b24d7c76de43c94cb
Braulio Rivas <brauliorivas2004@gmail.com>
2026-04-17 00:49:55 -0500

    copilotchat

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -6,7 +6,13 @@ return {
     },
     build = 'make tiktoken',
     opts = {
-      -- See Configuration section for options
+      temperature = 0.1, -- Lower = focused, higher = creative
+      trusted_tools = nil, -- Require approval for all tool calls
+      window = {
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+        width = 0.3,
+      },
+      auto_insert_mode = true, -- Enter insert mode when opening
     },
   },
 }
